0071e6443f6959d73022baf26c2db5ef095e8dc4
bremiy <luliu@vip.qq.com>
2019-08-04 12:51:58 +0800

    classtest.py

--- classtest.py
+++ classtest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-class UserData:
+class UserDate(object):
     def __init__(self,ID,Name):
         self.ID = ID
         self.Name = Name
@@ -8,13 +8,20 @@ class UserData:
     def __repr__(self):
         return 'ID:{} Name:{}'.format(self.ID, self.Name)
 
+class NewUser(UserDate):
+    def __init__(self,ID):
+        super()._inti_(ID)
 
+
+    def __inti__(self,Name):
+        set_name = Name
 
 
 
 if __name__=="__main__":
-    user1 = UserData(101,'Jack')
-    user2 = UserData(102,'Louplus')
+    user1 = NewUser(101,'Jack')
+    user1.set_name('Jackie')
+    user2 = NewUser(102,'Louplus')
     print(user1)
     print(user2)
 
