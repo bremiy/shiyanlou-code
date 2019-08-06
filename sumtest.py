080d92455b92c46477faec0cba0acf806be5d80d
bremiy <luliu@vip.qq.com>
2019-08-06 20:50:14 +0800

    sumtest.py

---
+++ sumtest.py
@@ -0,0 +1,10 @@
+a = input('please enter the first num:')
+b = input('please enter the secind num:')
+c = input('please enter the third num:')
+int(a)
+int(b)
+int(c)
+
+
+
+print('a','is',a,',b','is',b,',c','is',c,',','a+b+c'=a+b+c)
