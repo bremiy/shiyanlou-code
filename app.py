ae66d1b06bab34f9763d5175e1d9dab01794519a
bremiy <luliu@vip.qq.com>
2019-08-11 22:20:48 +0800

    22:20

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-
 from flask import request
 from flask import Flask
 from flask import url_for
 from flask import redirect
 
-
-
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return 'hello world'
+
+
 '''
 @app.route('/user/<username>')
 def user_index(username):
     return 'Hello {}'.format(username)
 '''
+
+
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
     return 'post{}'.format(post_id)
+
 
 @app.route('/courses/<name>')
 def courses_index(name):
@@ -35,12 +38,13 @@ def test():
     print(url_for('show_post', post_id=2, _anchor='a'))
     return 'test'
 
+
 @app.route('/<username>')
 def hello(username):
     if username == "shiyanlou":  # 如果访问/shiyanlou 则显示页面
         return 'hello {}'.format(username)
     else:
-        return redirect(url_for('index'))  #  否则重新定向到主页
+        return redirect(url_for('index'))  # 否则重新定向到主页
 
 
 @app.route('/user/<username>')
@@ -50,35 +54,37 @@ def user_index(username):
     print('----------------')
     print(request.args)
     print('----------------')
-    print('User-Agent:',request.headers.get('User-Agent'))
-    print('time',request.args.get('time'))
-    print('q',request.args.get('q'))
-    print('Q',request.args.get('Q'))
+    print('User-Agent:', request.headers.get('User-Agent'))
+    print('time', request.args.get('time'))
+    print('q', request.args.get('q'))
+    print('Q', request.args.get('Q'))
     return 'Hello {}'.format(username)
 
-@app.route('/register',methods=['GET','POST'])
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    '''
+    """
     print('method>:',request.method)
     print('name:',request.form.get('name'))
     print('password:',request.form.get('password'))
     print('hobbies:',request.form.getlist('hobbies'))
     print('age:',request.form,get('age',default=18))
-    '''
+    """
     if request.method == 'POST':
-        print('name:',request.form.get('name'))
-        print('password:',request.form.get('password'))
-        print('hobbies:',request.form.getlist('hobbies'))
-        print('age:',request.form.get('age',default=18))
+        print('name:', request.form.get('name'))
+        print('password:', request.form.get('password'))
+        print('hobbies:', request.form.getlist('hobbies'))
+        print('age:', request.form.get('age', default=18))
     return 'registered successfully!'
 
 
-@app.route('/httptest',request=['GET','POST'])
+@app.route('/httptest', request=['GET', 'POST'])
 def httptest():
-    
-    print('t',request.form.get('t'))
-    print('q',request.form.get('q'))
+    if request.method == 'POST':
+        print('t', request.form.get('t'))
+        print('q', request.form.get('q'))
     return 'It is get request!'
+
 
 if __name__ == '__main__':
     app.run()
